f6086fbeaef1f9419024ccc0f5c3f08a54a6241e
Jose Fernandez <jose@newrelic.com>
2013-05-31 21:35:26 -0700

    Add code comments

--- fog.js
+++ fog.js
@@ -20,6 +20,8 @@ ig.module(
       var fogColumns = [];
       var column = null;
 
+      // Iterate through each tile in our grid
+      // One column at a time, from left to right
       for (var x = 0; x < this.mapWidth; x++) {
         for (var y = 0; y < this.mapHeight; y++) {
           if(column === null) {
@@ -27,15 +29,20 @@ ig.module(
           }
 
           if (viewedTileCallback(x, y)) {
+            // We've run into a viewed tile
+            // Snip this fog column and store it in our buffer
             if(column.tiles > 0) {
               fogColumns.push(column);
             }
             column = null;
           } else if ((y + 1) === this.mapHeight) {
+            // We've reached the end of the column
+            // Store it and start a new one
             column.tiles++;
             fogColumns.push(column);
             column = null;
           } else {
+            // The fog column grows
             column.tiles++;
           }
         }
@@ -47,11 +54,13 @@ ig.module(
       }
     },
 
+    // Draws a vertical strip of fog using the 2D Canvas API
     _drawColumn: function(x, y, tiles) {
       ig.system.context.fillStyle = this.fillStyle;
       ig.system.context.fillRect(this._realSize(x), this._realSize(y), this._realSize(1), this._realSize(tiles));
     },
 
+    // Converts a location in our tile grid to the actual location in the game canvas
     _realSize: function(number) {
       return number * this.tileSize * ig.system.scale;
     }
